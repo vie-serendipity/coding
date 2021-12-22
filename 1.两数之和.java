bd34f5fa125209199e6b8c14af665083314111f6
vie-serendipity <zzy18851190927@gmail.com>
2021-12-22 19:48:05 +0800

    solve question 1 --java

---
+++ 1.两数之和.java
@@ -0,0 +1,25 @@
+
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length - 1; ++i) {
+            for (int j = i + 1; j < nums.length; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                }
+            }
+        }
+        return res;
+        
+    }
+}
+// @lc code=end
+
