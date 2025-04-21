44a6901b23fae39d2de05cdf2a1224d614cd4c65
vie-serendipity <2733147505@qq.com>
2025-04-21 21:11:17 +0800

    feat: problem 2145

---
+++ 2145.统计隐藏数组数目.go
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=2145 lang=golang
+ *
+ * [2145] 统计隐藏数组数目
+ */
+
+// @lc code=start
+func numberOfArrays(differences []int, lower int, upper int) int {
+	left, right := lower, upper
+	ans := upper - lower + 1
+	for i := 0; i < len(differences); i++ {
+		left = max(left+differences[i], lower)
+		right = min(differences[i]+right, upper)
+		ans = min(ans, right-left+1)
+	}
+	if ans<0{
+		return 0
+	}
+	return ans
+}
+
+// @lc code=end
+
